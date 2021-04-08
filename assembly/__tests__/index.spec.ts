f0c52abff0f92404a2e6e3bac1f6f5a9349210d5
Samuel Šulovský <samuel.sulovsky@gmail.com>
2021-04-08 19:43:33 +0200

    Refactored smart contract

--- assembly/__tests__/index.spec.ts
+++ assembly/__tests__/index.spec.ts
@@ -1,36 +1,22 @@
-import {PersistentVector, PersistentMap, u128} from 'near-sdk-as';
-import {init, depositFunds} from '../index';
-import {AccountId, Balance, Duration, TransactionId} from '../types';
-import {Deposit, Withdrawal, Refund} from '../models';
+import {u128} from 'near-sdk-as';
+import {NearBankableContract} from '../index';
 
 describe("Basics, Deposits, Withdrawals", () => {
+
     it("should initialise correctly", () => {
-        let contract = init("owner", new PersistentMap<TransactionId, Deposit>("deposits"),
-            new PersistentMap<TransactionId, Withdrawal>("withdrawals"), new PersistentMap<TransactionId, Refund>("pending-refunds"),
-            new PersistentMap<TransactionId, Refund>("approved-refunds"));
+        let contract = new NearBankableContract("memadrifttest1.testnet")
         expect(contract).not.toBeNull();
     });
 
-    it("should be able to get balance", () => {
-        let contract = init("owner", new PersistentMap<TransactionId, Deposit>("deposits"),
-            new PersistentMap<TransactionId, Withdrawal>("withdrawals"), new PersistentMap<TransactionId, Refund>("pending-refunds"),
-            new PersistentMap<TransactionId, Refund>("approved-refunds"));
+    it("owner should be set correctly", () => {
+        let contract = new NearBankableContract("memadrifttest1.testnet")
+        expect(contract.owner).toBe("memadrifttest1.testnet");
+    });
+
+    it("balance should be initialised correctly", () => {
+        let contract = new NearBankableContract("memadrifttest1.testnet")
         expect(contract.getBalance()).toBe(u128.Zero);
     });
 
-    /*
-     FIXME: How do we test this?
-    it("should be able to deposit", () => {
-        let contract = init("owner", new PersistentMap<TransactionId, Deposit>("deposits"),
-            new PersistentMap<TransactionId, Withdrawal>("withdrawals"), new PersistentMap<TransactionId, Refund>("pending-refunds"),
-            new PersistentMap<TransactionId, Refund>("approved-refunds"));
-        depositFunds({
-            blockchainTxId: "txid1",
-            amount: u128.One,
-            reasoncode: u128.One,
-            text: "test"
-        });
-        expect(contract.getDeposits().get(u128.One)).not.toBeNull();
-    });
-    */
+    // Transferring funds has to be tested via simulation testing in Rust
 });
